a321bd8e1c218e40a532728f01008177e59be8f9
mgk003 <mgk003@ucsd.edu>
2016-04-26 22:10:42 -0700

    added confirm password, email, and DOB. added validation check for confirm password (frontend only)

--- public/authentication/signup.js
+++ public/authentication/signup.js
@@ -10,5 +10,23 @@ function(AuthService){
 			username: me.id,
 			password: me.password
 		});
+    formValidation();
 	}
 }]);
+
+function formValidation() {
+  var pass = document.reg.password.value;
+  var confirmPass = document.reg.confirmPassword.value;
+  if(confirmPass_validation(pass,confirmPass)) {
+    alert("Sign up success!");
+  }
+}
+
+function confirmPass_validation(password, confirmPassword) {
+  if(password !== confirmPassword) {
+    alert("Password and Confirm Password do not match!");
+    console.log(`${password} !== ${confirmPassword}`);
+    return false;
+  }
+  return true;
+}
